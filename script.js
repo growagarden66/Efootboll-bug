577e5e236063d3e2a866298aff40d9e6299dd18f
growagarden66 <teo92943@gmail.com>
2025-11-10 12:32:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const btnTermos = document.getElementById("termos");
-const loading = document.getElementById("loading");
-const termosPage = document.getElementById("termosPage");
-
-btnTermos.addEventListener("click", () => {
-  document.querySelector(".container").style.display = "none";
-  loading.style.display = "block";
-
-  setTimeout(() => {
-    loading.style.display = "none";
-    termosPage.style.display = "block";
-  }, 3000); // 3 segundos de "carregando"
+document.getElementById('entrarBtn').addEventListener('click', () => {
+  window.location.href = 'login.html';
 });
